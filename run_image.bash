aded110657c5f17f582ee764fff7d898738484bd
dcentelles <centelld@uji.es>
2020-03-21 11:16:53 +0100

    update script

--- run_image.bash
+++ run_image.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+xhost +local:root_
+
 scriptPath=$(realpath $0)
 scriptDir=$(dirname $scriptPath)/
 share=$scriptDir/share
@@ -11,6 +13,7 @@ docker run -it \
     --env="DISPLAY=$DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --volume="$share:/root/share:rw" \
     --runtime=nvidia \
     --ulimit msgqueue=100000000:100000000 \
     $1 \
